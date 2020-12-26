e18cd6c7e41a03d6fe8532799cd27974942e1cc8
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-12-26 16:40:29 -0600

    Updates

---
+++ precache-manifest.82ca34e56aee838ece33e27c52419176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d6919e4caa157f66d7c985d723b83e",
+    "url": "/portfolio2.0/index.html"
+  },
+  {
+    "revision": "d776081798de496cee78",
+    "url": "/portfolio2.0/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "154df509308889daa1ad",
+    "url": "/portfolio2.0/static/css/main.286e6af1.chunk.css"
+  },
+  {
+    "revision": "d776081798de496cee78",
+    "url": "/portfolio2.0/static/js/2.93023178.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio2.0/static/js/2.93023178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154df509308889daa1ad",
+    "url": "/portfolio2.0/static/js/main.ba6c0e2c.chunk.js"
+  },
+  {
+    "revision": "d79510222c2212944bbc",
+    "url": "/portfolio2.0/static/js/runtime-main.5a4b770c.js"
+  }
+]);
